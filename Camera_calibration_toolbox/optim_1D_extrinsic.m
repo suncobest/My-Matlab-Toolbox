d392f6d681b8ae27eb506d56a45c021acd7fb263
ZhangPengfei <562386146@qq.com>
2017-08-23 00:11:27 +0800

    modify optim_1D_extrinsic

--- Camera_calibration_toolbox/optim_1D_extrinsic.m
+++ Camera_calibration_toolbox/optim_1D_extrinsic.m
@@ -16,14 +16,13 @@ Xp = reshape(extr_param,5,n_ima);
 Xp = gen_1D_points(Xp(1:3,:),Xp(4:5,:),rodlen);
 ex = []; % Global error vector
 for pp = ind_cam,
-    ii = (pp-1)*16;
     % load camera parameters
     fc = fc_mat(:,pp);
     cc = cc_mat(:,pp);
     alpha_c = alpha_vec(pp);
-    kc = intr_param(ii+6 : ii+10);
-    omwkk = intr_param(ii+11 : ii+13);
-    Twkk = intr_param(ii+14 : ii+16);
+    kc = kc_mat(:,pp);
+    omwkk = Omcc(:,pp);
+    Twkk = Tcc(:,pp);
     handkk = handcc(pp);
     active_view = active_imgviews(pp,:);
     kth = (find(active_view)-1)*n_cam+pp;
@@ -34,14 +33,6 @@ for pp = ind_cam,
     ex = [ex, ex_kk];
 end;
 err_std0 = std(ex,0,2);
-
-% The following vector helps to select the variables to update:
-selected_invar = [est_fc_mat; ones(2,1)*center_optim_vec; est_alpha_vec; est_dist_mat; active_cam(ones(6,1),:)];
-selected_invar(2,:) = selected_invar(2,:).*(est_aspect_ratio_vec | ~est_fc_mat(1,:));
-selected_invar(11:16,idm) = 0;
-selected_exvar = reshape(active_images(ones(5,1),:),1,nima5);
-ind_va = find(selected_invar);
-ind_vb = find(selected_exvar);
 
 lamda = 0.001; % set an initial value of the damping factor for the LM
 updateJ = 1;
@@ -55,10 +46,10 @@ for iter = 1:MaxIter,
     end;
     if updateJ,
         % JJ2 = JJ'*JJ = [U, W; W', V]
-        U = sparse([],[],[],ncam16,ncam16,16*ncam16);
+        U = sparse([],[],[],ncam6,ncam6,6*ncam6);
         V = sparse([],[],[],nima5,nima5,5*nima5);
-        W = sparse([],[],[],ncam16,nima5,ncam16*nima5);
-        ea = zeros(ncam16,1);        % A'*ex
+        W = sparse([],[],[],ncam6,nima5,ncam6*nima5);
+        ea = zeros(ncam6,1);        % A'*ex
         eb = zeros(nima5,1);         % B'*ex
 
         % generate 1D points on rod
@@ -67,15 +58,15 @@ for iter = 1:MaxIter,
         thph = Xp(4:5,:);
         [Xp,dXpdXo,dXpdtp] = gen_1D_points(Xo,thph,rodlen);
         for pp = ind_cam,
-            ii = (pp-1)*16;
+            ii = (pp-1)*6;
             % load camera parameters
-            fc = intr_param(ii+1 : ii+2);
-            cc = intr_param(ii+3 : ii+4);
-            alpha_c = intr_param(ii+5);
-            kc = intr_param(ii+6 : ii+10);
-            omwkk = intr_param(ii+11 : ii+13);
-            Twkk = intr_param(ii+14 : ii+16);
+            fc = fc_mat(:,pp);
+            cc = cc_mat(:,pp);
+            alpha_c = alpha_vec(pp);
+            kc = kc_mat(:,pp);
             handkk = handcc(pp);
+            omwkk = intr_param(ii+1 : ii+3);
+            Twkk = intr_param(ii+4 : ii+6);
 
             % load pixel points
             active_view = active_imgviews(pp,:);
@@ -83,14 +74,9 @@ for iter = 1:MaxIter,
             x_kk = cell2mat(x_cell(kth));
 
             ind = reshape(repmat(active_view,[np1D,1]),1,npts);
-            if est_aspect_ratio_vec(pp),
-                [x,dxdom,dxdT,dxdf,dxdc,dxdk,dxdalpha,dxdXp] = project_points_mirror2(Xp(:,ind),omwkk,Twkk,handkk,fc,cc,kc,alpha_c);
-            else
-                [x,dxdom,dxdT,dxdf,dxdc,dxdk,dxdalpha,dxdXp] = project_points_mirror2(Xp(:,ind),omwkk,Twkk,handkk,fc(1),cc,kc,alpha_c);
-                dxdf = repmat(dxdf,[1 2]);
-            end;
+            [x,dxdom,dxdT,~,~,~,~,dxdXp] = project_points_mirror2(Xp(:,ind),omwkk,Twkk,handkk,fc,cc,kc,alpha_c);
             ex_kk = x_kk - x;
-            Akk = [dxdf dxdc dxdalpha dxdk dxdom dxdT];
+            Akk = [dxdom dxdT];
 
             na = sum(active_view);
             nb = 2*na*np1D;
@@ -104,12 +90,11 @@ for iter = 1:MaxIter,
             for jj = 1:na,
                 Bkk(:,(jj-1)*5+1:jj*5) = [dxdXo(:,(jj-1)*3+1:jj*3),dxdtp(:,(jj-1)*2+1:jj*2)];
             end;
-            % Bkk = reshape([reshape(dxdXp*dXpdXo(idx,id3),[nb,3,na]), reshape(dxdXp*dXpdtp(idx,id2),[nb,2,na])], nb,5*na);
 
-            U(ii+1 : ii+16, ii+1 : ii+16) = Akk'*Akk;
-            ea(ii+1 : ii+16) = Akk'*ex_kk(:);
+            U(ii+1 : ii+6, ii+1 : ii+6) = Akk'*Akk;
+            ea(ii+1 : ii+6) = Akk'*ex_kk(:);
             V(id5, id5) = V(id5, id5) + Bkk'*Bkk;
-            W(ii+1 : ii+16, id5) = Akk'*Bkk;
+            W(ii+1 : ii+6, id5) = Akk'*Bkk;
             eb(id5) = eb(id5) + Bkk'*ex_kk(:);
         end;
         U = U(ind_va,ind_va);
@@ -135,24 +120,14 @@ for iter = 1:MaxIter,
     % compute reprojection error vector
     ex = [];
     for pp = ind_cam,
-        ii = (pp-1)*16;
-        % New intrinsic parameters:
-        if ~est_aspect_ratio_vec(pp) && all(est_fc_mat(:,pp)),
-            intr_update(ii+2) = intr_update(ii+1);
-        end;
-        fc = intr_update(ii+1 : ii+2);
-        cc = intr_update(ii+3 : ii+4);
-        alpha_c = intr_update(ii+5);
-        kc = intr_update(ii+6 : ii+10);
+        ii = (pp-1)*6;
+        fc = fc_mat(:,pp);
+        cc = cc_mat(:,pp);
+        alpha_c = alpha_vec(pp);
+        kc = kc_mat(:,pp);
         handkk = handcc(pp);
-        omwkk = intr_update(ii+11 : ii+13);
-        Twkk = intr_update(ii+14 : ii+16);
-        if center_optim_vec(pp) && (cc(1)<-.5 || cc(1)>imsize(1,pp)-.5 || cc(2)<-.5 || cc(2)>imsize(2,pp)-.5),
-            center_optim_vec(pp) = 0;
-            fprintf(1,['Warning: it appears that the principal point of camera %d'...
-                           'cannot be estimated.\nPlease run this code again!\n'], pp);
-            return;
-        end;
+        omwkk = intr_update(ii+1 : ii+3);
+        Twkk = intr_update(ii+4 : ii+6);
         active_view = active_imgviews(pp,:);
         kth = (find(active_view)-1)*n_cam+pp;
         x_kk = cell2mat(x_cell(kth));
@@ -184,13 +159,9 @@ end;
 fprintf(1,'\nEstimation of uncertainties...\n');
 
 % Extraction of the paramters for computing the reprojection error:
-intr_param = reshape(intr_param, 16, n_cam);
-fc_mat = intr_param(1:2,:);
-cc_mat = intr_param(3:4,:);
-alpha_vec = intr_param(5,:);
-kc_mat = intr_param(6:10,:);
-Omcc = intr_param(11:13,:);
-Tcc = intr_param(14:16,:);
+intr_param = reshape(intr_param, 6, n_cam);
+Omcc = intr_param(1:3,:);
+Tcc = intr_param(4:6,:);
 
 extr_param = reshape(extr_param, 5, n_ima);
 Xo = extr_param(1:3,:);
@@ -233,19 +204,13 @@ JJ2 = [U, W; W', V];    % JJ2 = JJ'*JJ
 JJ2_inv = speye(size(JJ2))/JJ2;
 
 % Extraction of the final intrinsic paramaters:
-param_error = zeros(ncam16,1);           % take value as perfect if not optimized (no error).
+param_error = zeros(ncam6,1);           % take value as perfect if not optimized (no error).
 nva = length(ind_va);
 param_error(ind_va) = 3*sqrt(abs(full(diag(JJ2_inv(1:nva,1:nva)))))*sigma_x;     % 3 sigma principle
-param_error = reshape(param_error,16,n_cam);
-ind = find(~est_aspect_ratio_vec & all(est_fc_mat,1));
-param_error(2,ind) = param_error(1,ind);
+param_error = reshape(param_error,6,n_cam);
 
-fc_mat_error = param_error(1:2,:);
-cc_mat_error = param_error(3:4,:);
-alpha_vec_error = param_error(5,:);
-kc_mat_error = param_error(6:10,:);
-Omcc_error = param_error(11:13,:);
-Tcc_error = param_error(14:16,:);
+Omcc_error = param_error(1:3,:);
+Tcc_error = param_error(4:6,:);
 
 % Extraction of the final extrinsic paramaters:
 param_error = zeros(nima5,1);
